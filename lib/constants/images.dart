0a395dc043f18329ab2712805e4a064c25bba1dc
ChrisnaBadar <badar.chrisna@gmail.com>
2023-05-24 14:19:04 +0700

    KNTR

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -59,4 +59,169 @@ class ImagePath {
   static const String PORTFOLIO_4 = "$imageDir/portfolio_04.jpg";
   static const String PORTFOLIO_5 = "$imageDir/portfolio_05.jpg";
   static const String PORTFOLIO_6 = "$imageDir/portfolio_06.jpg";
+
+  //sukajadi
+  static const String PORTOFOLIO_MASJID_SUKAJADI_1 =
+      '$imageDir/portofolio_masjid_sukajadi_1.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_2 =
+      '$imageDir/portofolio_masjid_sukajadi_2.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_3 =
+      '$imageDir/portofolio_masjid_sukajadi_3.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_4 =
+      '$imageDir/portofolio_masjid_sukajadi_4.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_5 =
+      '$imageDir/portofolio_masjid_sukajadi_5.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_6 =
+      '$imageDir/portofolio_masjid_sukajadi_6.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_7 =
+      '$imageDir/portofolio_masjid_sukajadi_7.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_8 =
+      '$imageDir/portofolio_masjid_sukajadi_8.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_9 =
+      '$imageDir/portofolio_masjid_sukajadi_9.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_10 =
+      '$imageDir/portofolio_masjid_sukajadi_10.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_11 =
+      '$imageDir/portofolio_masjid_sukajadi_11.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_12 =
+      '$imageDir/portofolio_masjid_sukajadi_12.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_13 =
+      '$imageDir/portofolio_masjid_sukajadi_13.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_14 =
+      '$imageDir/portofolio_masjid_sukajadi_14.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_15 =
+      '$imageDir/portofolio_masjid_sukajadi_15.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_16 =
+      '$imageDir/portofolio_masjid_sukajadi_16.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_17 =
+      '$imageDir/portofolio_masjid_sukajadi_17.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_18 =
+      '$imageDir/portofolio_masjid_sukajadi_18.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_19 =
+      '$imageDir/portofolio_masjid_sukajadi_19.jpg';
+  static const String PORTOFOLIO_MASJID_SUKAJADI_20 =
+      '$imageDir/portofolio_masjid_sukajadi_20.jpg';
+  //cianjur
+  static const String PORTOFOLIO_MASJID_CIANJUR_1 =
+      '$imageDir/portofolio_masjid_cianjur_1.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_2 =
+      '$imageDir/portofolio_masjid_cianjur_2.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_3 =
+      '$imageDir/portofolio_masjid_cianjur_3.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_4 =
+      '$imageDir/portofolio_masjid_cianjur_4.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_5 =
+      '$imageDir/portofolio_masjid_cianjur_5.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_6 =
+      '$imageDir/portofolio_masjid_cianjur_6.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_7 =
+      '$imageDir/portofolio_masjid_cianjur_7.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_8 =
+      '$imageDir/portofolio_masjid_cianjur_8.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_9 =
+      '$imageDir/portofolio_masjid_cianjur_9.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_10 =
+      '$imageDir/portofolio_masjid_cianjur_10.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_11 =
+      '$imageDir/portofolio_masjid_cianjur_11.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_12 =
+      '$imageDir/portofolio_masjid_cianjur_12.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_13 =
+      '$imageDir/portofolio_masjid_cianjur_13.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_14 =
+      '$imageDir/portofolio_masjid_cianjur_14.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_15 =
+      '$imageDir/portofolio_masjid_cianjur_15.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_16 =
+      '$imageDir/portofolio_masjid_cianjur_16.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_17 =
+      '$imageDir/portofolio_masjid_cianjur_17.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_18 =
+      '$imageDir/portofolio_masjid_cianjur_18.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_19 =
+      '$imageDir/portofolio_masjid_cianjur_19.jpg';
+  static const String PORTOFOLIO_MASJID_CIANJUR_20 =
+      '$imageDir/portofolio_masjid_cianjur_20.jpg';
+  //stai
+  static const String PORTOFOLIO_GEDUNG_STAI_1 =
+      '$imageDir/portofolio_gedung_stai_1.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_2 =
+      '$imageDir/portofolio_gedung_stai_2.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_3 =
+      '$imageDir/portofolio_gedung_stai_3.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_4 =
+      '$imageDir/portofolio_gedung_stai_4.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_5 =
+      '$imageDir/portofolio_gedung_stai_5.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_6 =
+      '$imageDir/portofolio_gedung_stai_6.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_7 =
+      '$imageDir/portofolio_gedung_stai_7.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_8 =
+      '$imageDir/portofolio_gedung_stai_8.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_9 =
+      '$imageDir/portofolio_gedung_stai_9.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_10 =
+      '$imageDir/portofolio_gedung_stai_10.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_11 =
+      '$imageDir/portofolio_gedung_stai_11.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_12 =
+      '$imageDir/portofolio_gedung_stai_12.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_13 =
+      '$imageDir/portofolio_gedung_stai_13.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_14 =
+      '$imageDir/portofolio_gedung_stai_14.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_15 =
+      '$imageDir/portofolio_gedung_stai_15.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_16 =
+      '$imageDir/portofolio_gedung_stai_16.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_17 =
+      '$imageDir/portofolio_gedung_stai_17.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_18 =
+      '$imageDir/portofolio_gedung_stai_18.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_19 =
+      '$imageDir/portofolio_gedung_stai_19.jpg';
+  static const String PORTOFOLIO_GEDUNG_STAI_20 =
+      '$imageDir/portofolio_gedung_stai_20.jpg';
+  //salatiga
+  static const String PORTOFOLIO_DTP_SALATIGA_1 =
+      '$imageDir/portofolio_dtp_salatiga_1.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_2 =
+      '$imageDir/portofolio_dtp_salatiga_2.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_3 =
+      '$imageDir/portofolio_dtp_salatiga_3.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_4 =
+      '$imageDir/portofolio_dtp_salatiga_4.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_5 =
+      '$imageDir/portofolio_dtp_salatiga_5.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_6 =
+      '$imageDir/portofolio_dtp_salatiga_6.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_7 =
+      '$imageDir/portofolio_dtp_salatiga_7.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_8 =
+      '$imageDir/portofolio_dtp_salatiga_8.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_9 =
+      '$imageDir/portofolio_dtp_salatiga_9.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_10 =
+      '$imageDir/portofolio_dtp_salatiga_10.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_11 =
+      '$imageDir/portofolio_dtp_salatiga_11.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_12 =
+      '$imageDir/portofolio_dtp_salatiga_12.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_13 =
+      '$imageDir/portofolio_dtp_salatiga_13.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_14 =
+      '$imageDir/portofolio_dtp_salatiga_14.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_15 =
+      '$imageDir/portofolio_dtp_salatiga_15.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_16 =
+      '$imageDir/portofolio_dtp_salatiga_16.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_17 =
+      '$imageDir/portofolio_dtp_salatiga_17.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_18 =
+      '$imageDir/portofolio_dtp_salatiga_18.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_19 =
+      '$imageDir/portofolio_dtp_salatiga_19.jpg';
+  static const String PORTOFOLIO_DTP_SALATIGA_20 =
+      '$imageDir/portofolio_dtp_salatiga_20.jpg';
 }
